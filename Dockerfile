70a51d9e904eedf18e344e8f926a90c9e4090cae
Ruben-David <ruben-david.kraus@ottogroup.com>
2023-07-28 13:36:30 +0200

    updated shit

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
-FROM rust:1.70 AS builder
-WORKDIR /usr/src/api
-COPY ./todo-api .
-RUN cargo install --path .
-RUN ls /usr/local/cargo/bin
+FROM rust
 
-FROM debian:buster-slim
-RUN apt-get update & apt-get install -y extra-runtime-dependencies & rm -rf /var/lib/apt/lists/*
-COPY --from=builder /usr/local/cargo/bin/api /usr/local/bin/api
-CMD ["api"]
+WORKDIR /usr/src/api
+
+COPY ./todo-api .
+
+CMD ["cargo","run"]
---
+++ k6/test.js
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function () {
+  // Test the list_todos_handler() endpoint
+  http.get('http://localhost:8000/');
+  sleep(1);
+
+  // Test the list_todo_handler(todo_id) endpoint
+  http.get('http://localhost:8000/todo/1'); // Replace '1' with an actual todo_id
+  sleep(1);
+
+  // Test the done_todo_handler(todo_id) endpoint
+  http.get('http://localhost:8000/done/1'); // Replace '1' with an actual todo_id
+  sleep(1);
+
+  // Test the create_todo_handler(todo) endpoint
+  const todo = {
+    // Replace with actual JSON data for a new todo
+  };
+  http.post('http://localhost:8000/new_todo', JSON.stringify(todo), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  sleep(1);
+
+  // Test the delete_todo_handler(todo_id) endpoint
+  http.get('http://localhost:8000/delete/1'); // Replace '1' with an actual todo_id
+  sleep(1);
+}
