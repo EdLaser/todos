501626e9e5c9acaedf172b67c0f02dc1f5cad0ca
Ruben-David Kraus <s80350@htw-dresden.de>
2023-08-07 11:20:17 +0200

    updated to test

--- k6/test.js
+++ k6/test.js
@@ -8,22 +8,22 @@ export let options = {
 
 export default function () {
   // Test the list_todos_handler() endpoint
-  http.get('http://localhost:8000/');
+  http.get('http://web:8000/');
   sleep(1);
 
   // Test the list_todo_handler(todo_id) endpoint
-  http.get('http://localhost:8000/todo/1'); // Replace '1' with an actual todo_id
+  http.get('http://web:8000/todo/1'); // Replace '1' with an actual todo_id
   sleep(1);
 
   // Test the done_todo_handler(todo_id) endpoint
-  http.get('http://localhost:8000/done/1'); // Replace '1' with an actual todo_id
+  http.get('http://web:8000/done/1'); // Replace '1' with an actual todo_id
   sleep(1);
 
   // Test the create_todo_handler(todo) endpoint
   const todo = {
     // Replace with actual JSON data for a new todo
   };
-  http.post('http://localhost:8000/new_todo', JSON.stringify(todo), {
+  http.post('http://web:8000/new_todo', JSON.stringify(todo), {
     headers: {
       'Content-Type': 'application/json',
     },
@@ -31,6 +31,6 @@ export default function () {
   sleep(1);
 
   // Test the delete_todo_handler(todo_id) endpoint
-  http.get('http://localhost:8000/delete/1'); // Replace '1' with an actual todo_id
+  http.get('http://web:8000/delete/1'); // Replace '1' with an actual todo_id
   sleep(1);
 }
