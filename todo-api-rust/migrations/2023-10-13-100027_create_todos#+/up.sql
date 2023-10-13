ddaeba488b904ea47931ea1ce082b72e2319f49a
Ruben Kraus <s80350@htw-dresden.de>
2023-10-13 12:02:09 +0200

    updated sql statements

--- todo-api-rust/migrations/2023-10-13-100027_create_todos#+/up.sql
+++ todo-api-rust/migrations/2023-10-13-100027_create_todos#+/up.sql
@@ -1,1 +1,7 @@
 -- Your SQL goes here
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    done BOOLEAN NOT NULL DEFAULT FALSE
+);
